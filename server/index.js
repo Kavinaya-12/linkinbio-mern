cadad4e7504ed5c0056551b89431716681cc0370
kavinaya-12 <kavinaya1201@gmail.com>
2025-12-01 19:49:06 +0530

    prepare backend deployment

--- server/index.js
+++ server/index.js
@@ -7,17 +7,29 @@ const authRoutes = require("./routes/auth");
 const userRoutes = require("./routes/user");
 
 const app = express();
-app.use(cors());
+
+app.use(
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://linkinbio-mern-3h5jls7ts-kavinayas-projects.vercel.app"
+    ],
+    credentials: true,
+  })
+);
+
 app.use(express.json());
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/user", userRoutes);
 
-mongoose.connect(process.env.MONGO_URI)
+mongoose
+  .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected successfully");
     app.listen(process.env.PORT, () =>
       console.log(`Server running on http://localhost:${process.env.PORT}`)
     );
   })
-  .catch(err => console.error("MongoDB Connection Error:", err));
+  .catch((err) => console.error("MongoDB Connection Error:", err));
