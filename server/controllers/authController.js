9a06c93382fc9c6b801d71f2ac800d169a1d941d
kavinaya-12 <kavinaya1201@gmail.com>
2025-12-01 16:07:16 +0530

    prepare backend deployment

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -29,24 +29,28 @@ exports.signup = async (req, res) => {
     const token = generateToken(user._id);
     res.json({ token });
   } catch (err) {
-  console.error("Signup Error:", err);
-  res.status(500).json({ message: "Signup failed" });
-}
-
-
+    console.error("Signup Error:", err);
+    res.status(500).json({ message: "Signup failed" });
+  }
+}; 
 
 exports.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email });
-    if (!user) return res.status(400).json({ message: "User not found" });
+    if (!user)
+      return res.status(400).json({ message: "User not found" });
 
     const match = await bcrypt.compare(password, user.password);
-    if (!match) return res.status(400).json({ message: "Invalid credentials" });
+    if (!match)
+      return res.status(400).json({ message: "Invalid credentials" });
 
     const token = generateToken(user._id);
     res.json({ token });
+
   } catch (err) {
+    console.error("Login Error:", err);
     res.status(500).json({ message: "Login failed" });
   }
 };
