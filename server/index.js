2732094fbbec954f427b49863d7b5548c06f3e62
kavinaya-12 <kavinaya1201@gmail.com>
2025-12-03 20:39:48 +0530

    public/production link change

--- server/index.js
+++ server/index.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 
 const app = express();
 
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ limit: "10mb", extended: true }));
+app.use(express.json({ limit: "50gb" }));
+app.use(express.urlencoded({ limit: "50gb", extended: true }));
 
 app.use(
   cors({
